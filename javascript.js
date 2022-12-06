1f765a51371f6f06314becc3dde1c06e9c322876
Ilia Bochkov <ilya.bochkov97@gmail.com>
2022-12-06 19:18:08 +0200

    Implement animations in JS for read books switch

--- javascript.js
+++ javascript.js
@@ -1,39 +1,58 @@
 
 
-function Book(title, author, pages, year, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.year = year;
-    this.read = read;
-}
+// document.querySelectorAll('.some-class').forEach(item => {
+//     item.addEventListener('click', event => {
+//       //handle click
+//     })
+//   })
 
-Book.prototype.generateHTML = function() {
-    const book_html_template = '';
-    return book_html_template;
-}
+const checkboxes = document.querySelectorAll('.read_checkbox');
 
-function BookShelf() {
-    this.bookCollection = [];
-}
+console.log(checkboxes);
 
-BookShelf.prototype.addBookToLibrary = function(book) {
-    this.bookCollection.push(book);
-}
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("click", e => {
+        checkbox.nextElementSibling.classList.toggle("read")
+        checkbox.closest(".book").classList.toggle("read")
+        
+    })
+})
 
-BookShelf.prototype.renderBooks = function() {
+
+
+// function Book(title, author, pages, year, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.year = year;
+//     this.read = read;
+// }
+
+// Book.prototype.generateHTML = function() {
+//     const book_html_template = '';
+//     return book_html_template;
+// }
+
+// function BookShelf() {
+//     this.initialBookCollection = [];
+//     this.sortedBookCollection = [];
+// }
+
+// BookShelf.prototype.addBookToLibrary = function(book) {
+//     this.initialBookCollection.push(book);
+// }
+
+// BookShelf.prototype.renderBooks = function() {
     
-}
+// }
 
 
-// 
+// const book1 = new Book("My life", "Ilia Bochkov", 200, 1997, true)
 
-const book1 = new Book("My life", "Ilia Bochkov", 200, 1997, true)
+// const bookShelf = new BookShelf();
 
-const bookShelf = new BookShelf();
+// bookShelf.addBookToLibrary(book1);
+// bookShelf.addBookToLibrary(book1);
+// bookShelf.addBookToLibrary(book1);
 
-bookShelf.addBookToLibrary(book1);
-bookShelf.addBookToLibrary(book1);
-bookShelf.addBookToLibrary(book1);
-
-console.log(bookShelf.bookCollection[0]);
+// console.log(bookShelf.initialBookCollection[0]);
