1c94f8a189f9cb5d15c8297ccf22303aeca15c93
Ilia Bochkov <ilya.bochkov97@gmail.com>
2022-12-06 19:58:05 +0200

    Implement HTML code rendering for JS books objects and implement close button

--- javascript.js
+++ javascript.js
@@ -6,9 +6,65 @@
 //     })
 //   })
 
-const checkboxes = document.querySelectorAll('.read_checkbox');
+function Book(title, author, pages, language, year, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.language = language;
+    this.year = year;
+    this.read = read;
+}
 
-console.log(checkboxes);
+Book.prototype.generateHTML = function(bookNumber) {
+    console.log(this.read)
+    let read = "";
+    this.read ? read = "checked" : "";
+    const book_html_template = `
+    <div class="book" id="book${bookNumber}">
+        <button class="close"><span class="material-icons remove-book"> close </span></button>
+        <h4 id="title">${this.title}</h4>
+        <p>By: <span id="author">${this.author}</span></p>
+        <p>Number of pages: <span id="pages">${this.pages}</span></p>
+        <p>Language: <span id="language">${this.language}</span></p>
+        <p>Published: <span id="pubYear">${this.year}</span></p>
+        <div class="reading-state">
+            <span>Mark as read: </span>
+            <label for="reading-input1">
+                <input ${read} type="checkbox" class="read_checkbox" id="reading-input1">
+                <span class="control"></span>
+            </label>
+        </div>
+    </div>`;
+    return book_html_template;
+}
+
+function BookShelf() {
+    this.initialBookCollection = [];
+    this.sortedBookCollection = [];
+}
+
+BookShelf.prototype.addBookToLibrary = function(book) {
+    this.initialBookCollection.push(book);
+}
+
+BookShelf.prototype.renderBooks = function() {
+    
+}
+
+
+const book1 = new Book("Book of Knowledge: Story of Fire and Ice and Stuff", "Ilia Bochkov", 200, "English", 1997, false)
+
+const bookShelf = new BookShelf();
+
+bookShelf.addBookToLibrary(book1);
+bookShelf.addBookToLibrary(book1);
+bookShelf.addBookToLibrary(book1);
+
+bookShelf.initialBookCollection[2].read = true;
+
+////
+
+const checkboxes = document.querySelectorAll('.read_checkbox');
 
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener("click", e => {
@@ -18,41 +74,12 @@ checkboxes.forEach(checkbox => {
     })
 })
 
+const closeButtons = document.querySelectorAll('.close');
 
+closeButtons.forEach(closeButton => {
+    closeButton.addEventListener("click", e => {
+        closeButton.closest(".book").remove();
+    })
+})
 
-// function Book(title, author, pages, year, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.year = year;
-//     this.read = read;
-// }
-
-// Book.prototype.generateHTML = function() {
-//     const book_html_template = '';
-//     return book_html_template;
-// }
-
-// function BookShelf() {
-//     this.initialBookCollection = [];
-//     this.sortedBookCollection = [];
-// }
-
-// BookShelf.prototype.addBookToLibrary = function(book) {
-//     this.initialBookCollection.push(book);
-// }
-
-// BookShelf.prototype.renderBooks = function() {
-    
-// }
-
-
-// const book1 = new Book("My life", "Ilia Bochkov", 200, 1997, true)
-
-// const bookShelf = new BookShelf();
-
-// bookShelf.addBookToLibrary(book1);
-// bookShelf.addBookToLibrary(book1);
-// bookShelf.addBookToLibrary(book1);
-
-// console.log(bookShelf.initialBookCollection[0]);
+console.log(book1.generateHTML(1));
