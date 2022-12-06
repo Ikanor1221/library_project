c6e8f48053ff9322853cb6b61551bc85d75443ca
Ilia Bochkov <ilya.bochkov97@gmail.com>
2022-12-06 20:48:50 +0200

    Implement full rendering of books only from JS

--- javascript.js
+++ javascript.js
@@ -13,6 +13,7 @@ function Book(title, author, pages, language, year, read) {
     this.language = language;
     this.year = year;
     this.read = read;
+    // this.bookNumber = bookNumber;
 }
 
 Book.prototype.generateHTML = function(bookNumber) {
@@ -29,8 +30,8 @@ Book.prototype.generateHTML = function(bookNumber) {
         <p>Published: <span id="pubYear">${this.year}</span></p>
         <div class="reading-state">
             <span>Mark as read: </span>
-            <label for="reading-input1">
-                <input ${read} type="checkbox" class="read_checkbox" id="reading-input1">
+            <label for="reading-input${bookNumber}">
+                <input ${read} type="checkbox" class="read_checkbox" id="reading-input${bookNumber}">
                 <span class="control"></span>
             </label>
         </div>
@@ -48,19 +49,28 @@ BookShelf.prototype.addBookToLibrary = function(book) {
 }
 
 BookShelf.prototype.renderBooks = function() {
+
+    const display = document.querySelector('.book-display');
+    for (const book in this.initialBookCollection) {
+        console.log(book)
+        display.insertAdjacentHTML("beforeend", this.initialBookCollection[book].generateHTML(book))
+    }
+
     
 }
 
 
-const book1 = new Book("Book of Knowledge: Story of Fire and Ice and Stuff", "Ilia Bochkov", 200, "English", 1997, false)
+const book1 = new Book("Book of Knowledge Part 1: Story of Fire and Ice", "Ilia Bochkov", 200, "English", 1997, false)
+const book2 = new Book("Book of Knowledge Part 2: Story of Fire and Ice and Stuff", "Ilia Bochkov", 200, "English", 1999, false)
+const book3 = new Book("Book of Knowledge Part 3: Story of Fire and Ice and More Stuff", "Ilia Bochkov", 200, "English", 2004, true)
 
 const bookShelf = new BookShelf();
 
 bookShelf.addBookToLibrary(book1);
-bookShelf.addBookToLibrary(book1);
-bookShelf.addBookToLibrary(book1);
+bookShelf.addBookToLibrary(book2);
+bookShelf.addBookToLibrary(book3);
 
-bookShelf.initialBookCollection[2].read = true;
+bookShelf.renderBooks();
 
 ////
 
@@ -69,8 +79,7 @@ const checkboxes = document.querySelectorAll('.read_checkbox');
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener("click", e => {
         checkbox.nextElementSibling.classList.toggle("read")
-        checkbox.closest(".book").classList.toggle("read")
-        
+        checkbox.closest(".book").classList.toggle("read")      
     })
 })
 
@@ -82,4 +91,3 @@ closeButtons.forEach(closeButton => {
     })
 })
 
-console.log(book1.generateHTML(1));
